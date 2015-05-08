721be140e69c8b715793acd0518b43345edc26b1
Joaquin Casanova <joaquin.casanova@gmail.com>
2015-05-08 19:12:13 +0000

    python

--- flow.py
+++ flow.py
@@ -7,35 +7,53 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
-global solPin=15#BCM 22
-global floPin=11#BCM 17
-global tickVol=206#1000mL/485 pulses 2.06 mL/pulse
+global solPin
+solPin=15#BCM 22
+global floPin
+floPin=11#BCM 17
+global tickVol
+tickvol=2.06#1000mL/485 pulses 2.06 mL/pulse
 
 GPIO.setup(solPin, GPIO.OUT)
 GPIO.setup(floPin, GPIO.IN)
 
 class flo():
+
     def __init__(self):
         self.clicks=0
         self.last=time.time()
         self.now=time.time()
-    def update():  
-        self.clicks++
-        self.last=self.now()
-        self.now()=time.time()
-    def clear():
+        self.start=time.time()
+        GPIO.add_event_detect(floPin, GPIO.RISING, callback=self.update)
+
+    def update(self, channel):  
+        self.clicks=self.clicks+1
+        self.last=self.now
+        self.now=time.time()
+        print(str(self.vol()) +" mL in "+ str(self.delta()) + " seconds " + str(self.clicks) + " on channel " + str(channel))
+
+    def clear(self):
         self.clicks=0
         self.last=time.time()
         self.now=time.time()
-    def vol():
-        return 2.06*self.clicks
-    def delta():
-        return self.now-self.last
+        self.start=time.time()
 
-meter = flo()
-GPIO.add_event_detect(floPin, GPIO.RISING, callback=flo.update)
+    def vol(self):
+        return tickvol*self.clicks
 
-while
+    def delta(self):
+        return self.now-self.start
+
+f = flo()
+
+while True:
+    dose=input("what volume? in mL.")
+    GPIO.output(solPin, False)
+    while f.vol()<dose:
+        GPIO.output(solPin, True)
+        
+    f.clear()
+
     
 
 
